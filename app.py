8ecaad10a9c4eaf2270dc078dc222da1f3a6739c
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-04-11 00:01:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,25 @@ from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 st.set_page_config(page_title="Document Genie", layout="wide")
 
 st.markdown("""
-## Document Genie: Get instant insights from your Documents
+## Assembly Ally: Your AI Assistant for Vehicle Assembly & Repair 🚗🔧
 
-This chatbot is built using the Retrieval-Augmented Generation (RAG) framework, leveraging Google's Generative AI model Gemini-PRO. It processes uploaded PDF documents by breaking them down into manageable chunks, creates a searchable vector store, and generates accurate answers to user queries. This advanced approach ensures high-quality, contextually relevant responses for an efficient and effective user experience.
+**Simplify Complex Procedures with AI-Powered Guidance 🧠**
 
-### How It Works
+Assembly Ally is your intelligent assistant for navigating vehicle assembly and repair manuals. Using advanced AI, it helps mechanics understand instructions, find information quickly, and complete tasks efficiently. 
 
-Follow these simple steps to interact with the chatbot:
+### How it Works
 
-1. **Enter Your API Key**: You'll need a Google API key for the chatbot to access Google's Generative AI models. Obtain your API key https://makersuite.google.com/app/apikey.
+1. **Upload Your Manuals:** Upload any type of instructional manual – PDFs, images, even handwritten notes. Assembly Ally can process diverse formats. 📄📑
+2. **Ask Your Question:**  Ask anything related to the assembly or repair process. Assembly Ally's AI will interpret your query and provide accurate answers. ❓💬
+3. **Get Instant Insights:** Extract key details like part names, numbers, specifications, and more. Assembly Ally can even summarize sections for you.  🔍💡
+4. **Learn as You Go:** Assembly Ally adapts to your needs, personalizing the experience based on your interactions and comprehension level. 📈😊
 
-2. **Upload Your Documents**: The system accepts multiple PDF files at once, analyzing the content to provide comprehensive insights.
-
-3. **Ask a Question**: After processing the documents, ask any question related to the content of your uploaded documents for a precise answer.
+**Get ready to experience a smarter way to work with Assembly Ally!** 🚀
 """)
 
 
@@ -48,9 +46,8 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", 
-                                                google_api_key=os.getenv("GOOGLE_API_KEY"))
+def get_vector_store(text_chunks, api_key):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
@@ -63,15 +60,13 @@ def get_conversational_chain():
 
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, 
-                                    google_api_key=os.getenv("GOOGLE_API_KEY"))
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key=api_key)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
 
 def user_input(user_question, api_key):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", 
-                                                google_api_key=os.getenv("GOOGLE_API_KEY"))
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
     try:
         new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
         docs = new_db.similarity_search(user_question)
